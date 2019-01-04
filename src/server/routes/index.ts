6747410376c585dd3d4f8ee976100baea560e96a
Oleg Mayurchenko <v-olmayu@microsoft.com>
2019-01-04 12:19:31 +0300

    Added API route

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,20 @@
 import { Express, Request, Response } from "express";
+const data  = require('../data/index.json');
+// const front = require('../../../target/index.html');
+ 
 
 export default function initializeRoutes(app: Express){
-  app.get("/", (req:Request, res:Response):void => {
+  app.get("/demo", (req:Request, res:Response):void => {
     return res.render("index.pug");
   });
+
+  app.get("/api", (req:Request, res:Response):Response => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
+    return res;
+  });
+
+  // app.get("*", (req:Request, res:Response):any =>{
+  //   return res.sendFile(front);
+  // })
 }
