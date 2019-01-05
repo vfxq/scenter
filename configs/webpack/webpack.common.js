eeb353eade58c3b0b475af56c5ef5a77aa5d02de
Oleg Mayurchenko <v-olmayu@microsoft.com>
2019-01-05 21:07:03 +0300

    Added aliases v.2

---
+++ configs/webpack/webpack.common.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require ('extract-text-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const webpackConf = {
+  entry: [
+    'webpack-hot-middleware/client?path=/__webpack_hmr&reload=true',
+    './src/client/index.tsx',
+  ],
+  output: {
+    path: path.resolve(__dirname, "../../target"),
+    filename: 'bundle.js',
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.scss'],
+    plugins: [
+      new TsconfigPathsPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }, {
+        test: /.js$/,
+        loader: 'source-map-loader',
+        enforce: 'pre'
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(({
+          fallback: 'style-loader', 
+          use: ['css-loader', 'sass-loader']
+        }))
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
+
+module.exports = webpackConf;
