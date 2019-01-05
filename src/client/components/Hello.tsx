f98fea0d32b607f4ade6e888de50381812ab8c70
Oleg Mayurchenko <v-olmayu@microsoft.com>
2019-01-05 21:05:12 +0300

    Added aliases

--- src/client/components/Hello.tsx
+++ src/client/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { isMobile } from 'utils';
 
 interface Props {
   framework: string
@@ -7,6 +8,7 @@ interface Props {
 class Hello extends React.Component<Props, {}> {
   render(){
     const { framework } = this.props;
+    console.log('--isMobile:', isMobile());
     return (
       <>
         <h1>Hello {framework}</h1>
