feae22b204b5b1a52b60beb2b032303ac6f36da3
Oleg Mayurchenko <v-olmayu@microsoft.com>
2019-01-24 20:48:01 +0300

    Added a hook

--- src/client/components/contacts/index.tsx
+++ src/client/components/contacts/index.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import Section from '../shared/section';
 
-class Contacts extends React.Component {
-  render(){
-    return(
-      <Section>
-        <h1>Контакты</h1>
-      </Section>
-    )
-  }
+const Contacts = () => {
+  return (
+    <Section>
+      <h1>Контакты</h1>
+    </Section>
+  );
 }
 
 export default Contacts
--- src/client/components/help/helpItem.tsx
+++ src/client/components/help/helpItem.tsx
@@ -8,60 +8,44 @@ interface IProps{
   }
 }
 
-class HelpItem extends React.Component<IProps, any>{
-  constructor(props:IProps) {
-    super(props);
-    this.state = {
-      show: false
-    };
+export default function HelpItem(props: IProps){
+  const [show, setShow] = React.useState(false);
+
+  const togglePopup = () => {
+    setShow(!show)
   }
-
-  public togglePopup = () => {
-    this.setState((state:any) => (
-      {show: !state.show}
-    ));
-  }
-
- 
-
-  public popup = () => {
-    return (
+  
+  const popUpBody = () => { 
+    return ( 
       <div className="popupHelpItem">
-        <div className="popupHelpItem__bground" onClick={this.togglePopup}>
-          
-        </div>
-        <div className="popupHelpItem__container">
-          <div className="popupHelpItem__close" onClick={this.togglePopup}>
-            close
-          </div>
-          <div>
-            <div className="popupHelpItem__content">
-            <h4>{this.props.data.title}</h4>
-            {this.props.data.extended}
+        <div className="popupHelpItem__bground" onClick={togglePopup}>
+          <div className="popupHelpItem__container">
+            <div className="popupHelpItem__close" onClick={togglePopup}>
+              close
+            </div>
+            <div>
+              <div className="popupHelpItem__content">
+                <h4>{props.data.title}</h4>
+                {props.data.extended}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+  )};
 
-  render(){
-    const {id, title} = this.props.data;
-    const popup = this.state.show ? this.popup() : null;
-
-    return (
-      <>
-        <div onClick={this.togglePopup}>
-        
-          <img src={require(`./img/${id}-min.png`)}/>
-          <p>
-            {title}
-          </p>
-        </div>
-        {popup}
-      </>
-    )
-  }
+  const { title, id } = props.data;
+  
+  const popup = show ? popUpBody() : null;
+  return(
+    <>
+      <div onClick={togglePopup}>
+        <img src={require(`./img/${id}-min.png`)}/>
+        <p>
+          {title}
+        </p>
+      </div>
+      {popup}   
+    </>
+  )
 }
-
-export default HelpItem;
